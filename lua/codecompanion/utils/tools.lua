1461e2e613164daabcbf93d63e2fe846ff65ba9d
Vlad Meșco <alzwded@gmail.com>
2025-10-01 06:05:41 +0000

    fix #2179
    Parameter quoting rules on Windows are complicated, because
    Windows programs only receive one big string with all arguments
    which they or msvcrt split back into argv. libuv used by NeoVim
    tries to be helpful, but ends up sending literal quotes to cmd.exe
    (programs get `"C:\file.txt"` with quotes instead of `C:\file.txt`)
    or not literal enough to powershell (literal empty arguments `""` get
    lost along the way)
    
    What does work, it spelling out command invocations in a batch file
    and letting cmd.exe figure things out. This way, libuv only gets
    involved in running `cmd.exe /c "temp.bat"`
    
    This does have the slight downside that if the LLM wants to run cmd
    `for ... %i ...` commands, they will fail, but this can be worked
    around with prompting.

--- lua/codecompanion/utils/tools.lua
+++ lua/codecompanion/utils/tools.lua
@@ -7,11 +7,47 @@ local M = {}
 ---@param args string[]
 ---@return string[]
 function M.build_shell_command(args)
-  return {
-    (vim.fn.has("win32") == 1 and "cmd.exe" or "sh"),
-    vim.fn.has("win32") == 1 and "/c" or "-c",
-    table.concat(args, " "),
-  }
+  if vim.fn.has("win32") == 1 then
+    -- Parameter quoting rules on Windows are complicated, because
+    -- Windows programs only receive one big string with all arguments
+    -- which they or msvcrt split back into argv. libuv used by NeoVim
+    -- tries to be helpful, but ends up sending literal quotes to cmd.exe
+    -- (programs get `"C:\file.txt"` with quotes instead of `C:\file.txt`)
+    -- or not literal enough to powershell (literal empty arguments `""` get
+    -- lost along the way)
+    --
+    -- What does work, it spelling out command invocations in a batch file
+    -- and letting cmd.exe figure things out. This way, libuv only gets
+    -- involved in running `cmd.exe /c "temp.bat"`
+    --
+    -- This does have the slight downside that if the LLM wants to run cmd
+    -- `for ... %i ...` commands, they will fail, but this can be worked
+    -- around with prompting.
+    local fname = vim.fn.tempname() .. ".bat"
+    local file = io.open(fname, "w")
+    if file then
+      file:write("@echo off", "\n")
+      file:write(table.concat(args, " "), "\n")
+      file:close()
+      return {
+        "cmd.exe",
+        "/c",
+        fname
+      }
+    else
+      return {
+        "cmd.exe",
+        "/c",
+        table.concat(args, " ")
+      }
+    end
+  else
+    return {
+      "sh",
+      "-c",
+      table.concat(args, " "),
+    }
+  end
 end
 
 ---Strip any ANSI color codes which don't render in the chat buffer
